296075ef640133ba8166ed5c986799d65300bf12
Aconic <tygreman@gmail.com>
2017-04-03 16:03:44 +0300

    Initial commit

---
+++ src/homework-2/main/java/SearchPageTest.java
@@ -0,0 +1,34 @@
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class SearchPageTest
+{
+
+    public static void main(String[] args) throws InterruptedException
+    {
+        ChromeDriverManager.getInstance().setup();
+        WebDriver chromeDriver = new ChromeDriver();
+
+        chromeDriver.get("http://bing.com");
+        WebDriverWait driverWait = new WebDriverWait(chromeDriver,1);
+
+        WebElement searchField = chromeDriver.findElement(By.className("b_searchbox"));
+        searchField.sendKeys("Automation");
+
+        searchField.submit();
+        System.out.println(chromeDriver.getTitle());
+        List<WebElement> webElements = chromeDriver.findElements(By.cssSelector("h2"));
+        webElements.remove(webElements.size()-1);
+        webElements.remove(webElements.size()-1);
+        for (WebElement item : webElements) {
+            System.out.println(item.getText());
+        }
+        chromeDriver.close();
+    }
+}
